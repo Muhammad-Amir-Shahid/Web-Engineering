beb68d3dd3f9779964f9452d2efe6e112e83297e
Muhammad Amir Shahid <bulcpers0n0l@gmail.com>
2024-10-30 01:12:26 +0500

    lab-7

---
+++ lab-7/src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone:true,
+  imports: [FormGroup],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      // Implement authentication logic here
+      console.log('Login successful', { email, password });
+    }
+  }
+}
